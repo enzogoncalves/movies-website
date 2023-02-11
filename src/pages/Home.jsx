9e7b5ce604c135e21b687ac83576614a5ed8765c
enzogoncalves <enzomateusgonc@gmail.com>
2023-02-11 14:59:18 -0300

    verifying deploy error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Section } from '../components/Section/styles'
-import UpComingMovies from '../components/UpComingMovies'
+// import UpComingMovies from '../components/UpComingMovies'
 import Loading from '../components/Loading'
 import MediaCarousel from '../components/MediaCarousel'
 import { FaFireAlt, FaStar } from 'react-icons/fa'
@@ -9,7 +9,7 @@ const Home = () => {
   document.title = 'Home'
 
   const [trendingMovies, setTrendingMovies] = useState()
-  const [upcomingMovies, setUpcomingMovies] = useState()
+  // const [upcomingMovies, setUpcomingMovies] = useState()
   const [topRatedMovies, setTopRatedMovies] = useState()
 
   useEffect(() => {
@@ -17,19 +17,19 @@ const Home = () => {
       const topRatedMoviesFetch = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=c2b569d95e4b2013348fb2f4430655a5&language=en-US&page=1&region=US')
       const topRatedMoviesData = await topRatedMoviesFetch.json()
 
-      const upcomingMoviesFetch = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=c2b569d95e4b2013348fb2f4430655a5&region=US&page=1&per_page=5')
-      let upcomingMoviesData = await upcomingMoviesFetch.json()
+      // const upcomingMoviesFetch = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=c2b569d95e4b2013348fb2f4430655a5&region=US&page=1&per_page=5')
+      // let upcomingMoviesData = await upcomingMoviesFetch.json()
 
-      upcomingMoviesData = upcomingMoviesData.results.filter((movie) => {
-        return movie.backdrop_path !== null
-      })
+      // upcomingMoviesData = upcomingMoviesData.results.filter((movie) => {
+      //   return movie.backdrop_path !== null
+      // })
 
       const trendingMoviesFetch = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=c2b569d95e4b2013348fb2f4430655a5')
       const trendingMoviesData = await trendingMoviesFetch.json()
 
 
       setTrendingMovies(trendingMoviesData.results)
-      setUpcomingMovies(upcomingMoviesData.slice(0, 5))
+      // setUpcomingMovies(upcomingMoviesData.slice(0, 5))
       setTopRatedMovies(topRatedMoviesData.results)
     }
 
